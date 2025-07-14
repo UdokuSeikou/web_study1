0a0ee4f6070a73770892a08d6be56f551a29e48f
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-15 00:38:23 +0900

    get json date

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const { prompt } = require('./mymodule');
 
+const url = 'https://jsonplaceholder.typicode.com/todos/';
+
 async function main() {
-    const name = await prompt('あなたの名前は？');
-    console.log(`こんにちは、${name}さん！`);
+    const num = await prompt('number: ');
+    fetch(url + num)
+    .then(resp => resp.json())
+    .then(json =>{
+        console.log('ID: ' + json.id);
+        console.log('User ID: ' + json.userId);
+        console.log('Title: ' + json.title);
+        console.log('Completed: ' + json.completed);
+    })
 }
 
-main()
+main();
