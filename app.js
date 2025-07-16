29130a5806a0f959485fb6b12ad03e0e514ed524
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-16 22:09:28 +0900

    writeFileSync

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const fs = require('fs');
 const { prompt } = require('./mymodule');
-const { error } = require('console');
  
 async function main() {
     const msg = await prompt('please type: ');
-    fs.writeFile('./data.txt', msg, (err) =>{
-        if (err) {
-            console.error(err.message);
-            return;
-        }
-        console.log('save data.txt to write:');
+    try {
+        fs.writeFileSync('./data.txt', msg);
+        console.log('save data.txt to write: ');
         console.log(msg);
-    });
+    } catch (e) {
+        console.error(e.message);
+    }
 }
 
 main();
