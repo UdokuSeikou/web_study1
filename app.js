c2b7e403c5325f53de80bfa9088a9ed451c9bd80
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-16 22:01:05 +0900

    writeFile

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
+const fs = require('fs');
 const { prompt } = require('./mymodule');
+const { error } = require('console');
  
-const url = 'https://jsonplaceholder.typicode.com/todos/';
-
-const data = {
-    id: 201,
-    userid: 1001,
-    title: null,
-    completed: false
-};
-
-const options = {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: null
-};
-
-
 async function main() {
-    const title = await prompt('title: ');
-    data.title = title;
-    options.body = JSON.stringify(data);
-
-    const resp = await fetch(url, options);
-    if (resp.status == 201){
-        console.log('登録しました');
-        const json = await resp.json();
-        console.log(json);
-    } else {
-        console.log('登録に失敗しました');
-        console.log('status: ' + resp.status)
-    }
+    const msg = await prompt('please type: ');
+    fs.writeFile('./data.txt', msg, (err) =>{
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        console.log('save data.txt to write:');
+        console.log(msg);
+    });
 }
 
 main();
