073a5819feef62ee3bc1b62f87c9a6e00fcbb911
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-16 23:17:08 +0900

    readFileSync

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const { error } = require('console');
 const fs = require('fs');
  
 async function main() {
-    fs.readFile('./data.txt', (err, data) =>{
-        if (err) {
-            console.error(err.message);
-            return;
-        }
-        console.log('data.textの中身: ');
-        console.log(data.toString())
-    })
+    try {
+        const data = fs.readFileSync('./data.txt');
+        console.log('data.txtの内容: ');
+        console.log(data.toString());
+    } catch (err){
+        console.error(err.message);
+    }
 }
 
 main();
