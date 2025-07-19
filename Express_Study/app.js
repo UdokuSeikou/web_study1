d85c3456cc441162ce024071a5b65c3115372bcb
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-20 03:22:27 +0900

    express display html

--- Express_Study/app.js
+++ Express_Study/app.js
@@ -1,9 +1,16 @@
-const http = require('http');
+const express = require('express');
+const path = require('path')
 
-let server = http.createServer(
-    (req,res)=>{
-        res.end('Hello, Node.js!');
-    }
-)
+const app = express();
 
-server.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// //ルートの設定
+// app.get('/',(req, res)=>{
+//     res.send('Express!');
+// });
+
+//サーバーを起動
+app.listen(3000, ()=>{
+    console.log('Server up http://localhost:3000/');
+})
