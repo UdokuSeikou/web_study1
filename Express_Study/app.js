067584718f0eae8b55b28bc4e49f3d3ba1a531ac
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-21 00:06:20 +0900

    form reflection

--- Express_Study/app.js
+++ Express_Study/app.js
@@ -6,12 +6,12 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const indexRouter = require('./routes/index');
-app.use('/', indexRouter);
-
 //静的ファイルのルートを設定
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: false}));
 
+const indexRouter = require('./routes/index');
+app.use('/', indexRouter);
 
 //サーバーを起動
 app.listen(3000, ()=>{
--- Express_Study/routes/index.js
+++ Express_Study/routes/index.js
@@ -4,11 +4,22 @@ const router = express.Router();
 
 //ルートハンドラ
 router.get('/', (req, res)=>{
-    const data = {
-        title: 'Express-app',
-        message: 'これはサンプル'
-    };
-    res.render('index', data);
+    let opt = {
+        title: 'Express',
+        message: 'フォームを入力'
+    }
+    res.render('index', opt);
+});
+
+router.post('/', (req, res)=>{
+    let name = req.body.username;
+    let pass = req.body.pass;
+    let msg = 'name: ' + name + ', password: ' + pass;
+    let opt = {
+        title: 'Express',
+        message: msg
+    }
+    res.render('index', opt);
 });
 
 module.exports = router;
