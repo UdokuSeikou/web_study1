220ef6c57f4db057fc7fddf620a1953df0dca1e6
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-07-21 02:37:21 +0900

    data in array

--- Express_Study/routes/index.js
+++ Express_Study/routes/index.js
@@ -2,24 +2,20 @@ const express = require('express');
 const { title } = require('process');
 const router = express.Router();
 
+let data = [];
+
 //ルートハンドラ
 router.get('/', (req, res)=>{
     let opt = {
         title: 'Express',
-        message: 'フォームを入力'
+        data: data
     }
     res.render('index', opt);
 });
 
 router.post('/', (req, res)=>{
-    let name = req.body.username;
-    let pass = req.body.pass;
-    let msg = 'name: ' + name + ', password: ' + pass;
-    let opt = {
-        title: 'Express',
-        message: msg
-    }
-    res.render('index', opt);
+    data.unshift(req.body.msg);
+    res.redirect('/');
 });
 
 module.exports = router;
