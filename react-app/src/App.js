862a40c9d395c104dcf8d9539cd7f29dbb05d326
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-08-05 00:32:26 +0900

    flag

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,13 +9,37 @@ function Message(props) {
   return <p>{props.content}</p>;
 }
 
+function Alert(props) {
+  return (
+    <div className='alert'>
+      <h2>{props.title}</h2>
+      <p>{props.content}</p>
+    </div>
+  )
+}
+
+function Card(props) {
+  return (
+    <div className='card'>
+      <Title content={props.title} />
+      <Message content={props.content} />
+    </div>
+  )
+}
+
 function App() {
+  const queryParams = new URLSearchParams(window.location.search);
+  const flag = queryParams.get('flag') === 'true'; // trueならアラート、falseならカードを表示
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Title content="タイトルです"/>
-        <Message content="1つ目のメッセージ"/>
-        <Message content="別のメッセージ"/>
+      <h1>React Sample</h1>
+      <header className="container">
+        { flag ?
+          <Alert title="アラート" content="1つ目のメッセージ"/>
+          :
+          <Card title="カード" content="別のメッセージを表示"/>
+        }
       </header>
     </div>
   );
