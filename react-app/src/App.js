b3a497336183d5a721f5972c1c975f4c73586c17
UdokuSeikou <seikou.udoku.forgetin3days@gmail.com>
2025-08-07 22:24:42 +0900

    total

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const countUp = () => {
-    setCount(count + 1);
-  }
-  const countInit = () => {
-    setCount(0);
+  const [num, setNum] = useState(0);
+  const [total, setTotal] = useState(0);
+  const calc = () => {
+    let result = 0;
+    for (let i = 0; i < num; i++) {
+      result += i;
+    }
+    setTotal(result);
   }
 
   return (
@@ -16,10 +18,10 @@ function App() {
       <h1>React Sample</h1>
       <header className="container">
         <div className='card'>
-          <h3>数字をカウント</h3>
-          <p>{"count: "+count}</p>
-          <button onClick={countUp}>Count</button>
-          <button onClick={countInit}>Reset</button>
+          <h3>合計を算出</h3>
+          <h4>合計: {total}</h4>
+          <input type="number" value={num} onChange={(e) => setNum(e.target.value)} />
+          <button onClick={calc}>計算</button>
         </div>
       </header>
     </div>
